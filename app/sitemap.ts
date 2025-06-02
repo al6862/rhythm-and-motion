5e696dfe6da2a6b7e5d326955f206db4b56e7ecd
Amber Li <amber@human-nyc.com>
2025-06-01 23:11:14 -0400

    added robots

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,10 +1,13 @@
-import { MetadataRoute } from 'next'
+import { MetadataRoute } from "next";
 import { sanityFetch } from "@/sanity/lib/fetch";
-import { siteUrlsQuery } from '@/sanity/lib/queries';
+import { siteUrlsQuery } from "@/sanity/lib/queries";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const data = await sanityFetch({ query: siteUrlsQuery, params: {baseUrl: 'https://www.rhythmandmotion.com'} })
-  const { homepage, pages } = data
+  const data = await sanityFetch({
+    query: siteUrlsQuery,
+    params: { baseUrl: "https://www.rhythmandmotion.com" },
+  });
+  const { homepage, pages } = data;
 
-  return [homepage, ...pages]
+  return [homepage, ...pages];
 }

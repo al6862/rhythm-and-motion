bea21963d23ba28bad4520ab86b7ec220eef0787
Amber Li <amber@human-nyc.com>
2025-06-06 14:06:39 -0400

    moved eventoverlays outside of event so no longer in scrolltrigger

--- app/components/Timeline.tsx
+++ app/components/Timeline.tsx
@@ -12,6 +12,7 @@ import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
 import { useGSAP } from "@gsap/react";
 import { useState, useRef, useEffect } from "react";
+import React from "react";
 
 type TimelineProps = Extract<
   NonNullable<NonNullable<NonNullable<PageQueryResult>["page"]>["content"]>[0],
@@ -20,81 +21,254 @@ type TimelineProps = Extract<
 
 type EventContentProps = NonNullable<TimelineProps["events"]>[0];
 
-const Event = ({ eventContent }: { eventContent: EventContentProps }) => {
-  const { title, description, content, coverImage, secondaryImage } =
-    eventContent;
-  const overlayRef = useRef<HTMLDivElement>(null);
-  const eventRef = useRef<HTMLDivElement>(null);
-  const titleRef = useRef<HTMLHeadingElement>(null);
-  const textRef = useRef<HTMLHeadingElement>(null);
-  const textContainerRef = useRef<HTMLDivElement>(null);
-  const imageRef = useRef<HTMLImageElement>(null);
-  const overlayContentRef = useRef<HTMLDivElement>(null);
-  const [overlayIsHidden, setOverlayIsHidden] = useState(true);
+const Event = ({
+  eventContent,
+  index,
+  overlayIsHidden,
+  setOverlayIsHidden,
+  setActiveIndex,
+}: {
+  eventContent: EventContentProps;
+  index: number;
+  overlayIsHidden: boolean;
+  setOverlayIsHidden: React.Dispatch<React.SetStateAction<boolean>>;
+  setActiveIndex: React.Dispatch<React.SetStateAction<number>>;
+}) => {
+  const { title, description, coverImage } = eventContent;
   const handleClick = () => {
+    setActiveIndex(index);
     setOverlayIsHidden(!overlayIsHidden);
   };
 
   useGSAP(() => {
-    gsap.set(overlayRef.current, { autoAlpha: 0 });
+    gsap.set("#eventOverlay-" + index, { autoAlpha: 0 });
+  });
+
+  return (
+    <div id={`event-` + index} className="shrink-0">
+      {title && (
+        <div className="pl-[28.7rem] 2xl:pl-[38rem]">
+          <h3 className="title relative z-10 pb-[3.7rem] after:absolute after:inset-x-0 after:bottom-0 after:border-l after:border-dashed after:pb-[3.7rem] after:content-['']">
+            {title}
+          </h3>
+        </div>
+      )}
+      <div className="border-t border-dashed pl-[28.7rem] 2xl:pl-[38rem]">
+        <div className="textImage relative z-10 flex h-[50vh] gap-20 pt-[3.7rem] after:absolute after:inset-x-0 after:top-0 after:border-l after:border-dashed after:pb-[3.7rem] after:content-['']">
+          {description && (
+            <div className="max-w-[30.6rem] text-white">
+              <CustomPortableText value={description as PortableTextBlock[]} />
+            </div>
+          )}
+          {coverImage && coverImage.assetPath && (
+            <Image
+              src={coverImage.assetPath}
+              alt={coverImage.caption || "missing alt"}
+              width={800}
+              height={1000}
+              className="z-10 w-[40rem] cursor-pointer object-contain max-lg:translate-y-[-7.4rem] lg:pb-32"
+              onClick={handleClick}
+            />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const EventOverlay = ({
+  eventContent,
+  index,
+  overlayIsHidden,
+  setOverlayIsHidden,
+}: {
+  eventContent: EventContentProps;
+  index: number;
+  overlayIsHidden: boolean;
+  setOverlayIsHidden: React.Dispatch<React.SetStateAction<boolean>>;
+}) => {
+  const { title, description, content, coverImage, secondaryImage } =
+    eventContent;
+  const handleClick = () => {
+    setOverlayIsHidden(!overlayIsHidden);
+  };
+
+  return (
+    <div
+      id={`eventOverlay-` + index}
+      className="invisible fixed left-0 top-0 z-20 h-screen w-screen overflow-auto bg-brown text-white md:overflow-hidden"
+    >
+      <div className="md:hidden">
+        <div className="relative">
+          {coverImage && coverImage.assetPath && (
+            <Image
+              src={coverImage.assetPath}
+              alt={coverImage.caption || "missing alt"}
+              width={1600}
+              height={2000}
+              className="w-full"
+            />
+          )}
+          {title && (
+            <div className="absolute left-[1.6rem] top-1/2 -translate-y-1/2">
+              <h1>{title}</h1>
+            </div>
+          )}
+          <span
+            className="absolute right-[1.6rem] top-1/2 -translate-y-1/2 cursor-pointer"
+            onClick={handleClick}
+          >
+            Back
+          </span>
+        </div>
+        <div className="flex -translate-y-24 flex-col gap-16 px-[4.4rem]">
+          {description && (
+            <CustomPortableText value={description as PortableTextBlock[]} />
+          )}
+          {secondaryImage && secondaryImage.assetPath && (
+            <div className="flex justify-end gap-[1.6rem]">
+              <p className="caption h-fit max-w-44 self-end italic before:content-['('] after:content-[')']">
+                {secondaryImage.caption}
+              </p>
+              <Image
+                src={secondaryImage.assetPath}
+                alt={secondaryImage.caption || "missing alt"}
+                width={240}
+                height={320}
+                className="w-48 object-contain"
+              />
+            </div>
+          )}
+          {content && (
+            <CustomPortableText value={content as PortableTextBlock[]} />
+          )}
+        </div>
+      </div>
+      <div className="max-md:hidden">
+        {coverImage && coverImage.assetPath && (
+          <Image
+            src={coverImage.assetPath}
+            alt={coverImage.caption || "missing alt"}
+            width={800}
+            height={1000}
+            className="absolute left-32 top-1/2 w-[40rem] -translate-y-1/2 object-contain"
+          />
+        )}
+        <div className="absolute left-[19rem] top-1/2 flex gap-16 xl:left-[38rem]">
+          <div className="overlayContent flex translate-x-[-100px] translate-y-[-100px] gap-16 pr-[1.6rem]">
+            {description && (
+              <CustomPortableText value={description as PortableTextBlock[]} />
+            )}
+            {content && (
+              <CustomPortableText value={content as PortableTextBlock[]} />
+            )}
+          </div>
+        </div>
+
+        {title && (
+          <div className="absolute left-12 flex h-[50vh] items-end pb-[12.5rem] lg:left-40 xl:left-[34rem] 2xl:left-[54.6rem]">
+            <h1>{title}</h1>
+          </div>
+        )}
+        {secondaryImage && secondaryImage.assetPath && (
+          <div className="absolute right-12 top-[16.5rem] flex items-end gap-[1.6rem] lg:right-[22rem]">
+            <p className="caption max-w-44 italic before:content-['('] after:content-[')']">
+              {secondaryImage.caption}
+            </p>
+            <Image
+              src={secondaryImage.assetPath}
+              alt={secondaryImage.caption || "missing alt"}
+              width={240}
+              height={320}
+              className="w-48 object-contain"
+            />
+          </div>
+        )}
+        <button
+          className="absolute right-32 top-[12.5rem] cursor-pointer"
+          onClick={handleClick}
+        >
+          Back
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default function Timeline({ content }: { content: TimelineProps }) {
+  const { header, events, link } = content;
+  const timelineRef = useRef<HTMLDivElement>(null);
+  const buttonRef = useRef<HTMLDivElement>(null);
+  const logoRef = useRef<HTMLDivElement>(null);
+  const [overlayIsHidden, setOverlayIsHidden] = useState<boolean>(true);
+  const [activeIndex, setActiveIndex] = useState<number>(0);
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  useGSAP(() => {
+    gsap.to(timelineRef.current, {
+      xPercent: -100,
+      x: () => window.innerWidth,
+      ease: "none",
+      scrollTrigger: {
+        trigger: timelineRef.current,
+        pin: true,
+        end: () => `+=${timelineRef.current?.offsetWidth} bottom`,
+        scrub: 1,
+      },
+    });
   });
 
   useGSAP(
     () => {
       if (window.innerWidth < 1024) {
         if (overlayIsHidden) {
-          gsap.timeline().to(overlayRef.current, { autoAlpha: 0 }, "<");
+          gsap
+            .timeline()
+            .to("#eventOverlay-" + activeIndex, { autoAlpha: 0 }, "<");
         } else {
-          gsap.timeline().to(overlayRef.current, { autoAlpha: 1 }, "<");
+          gsap
+            .timeline()
+            .to("#eventOverlay-" + activeIndex, { autoAlpha: 1 }, "<");
         }
       } else {
         if (overlayIsHidden) {
           gsap
             .timeline()
-            .to(overlayContentRef.current, {
+            .to("#eventOverlay-" + activeIndex + " .overlayContent", {
               y: -100,
               x: -100,
               ease: "elastic.out(1,0.5) ",
               duration: 2,
             })
-            .to(overlayRef.current, { autoAlpha: 0 }, "<")
+            .to("#eventOverlay-" + activeIndex, { autoAlpha: 0 }, "<")
             .to(
-              imageRef.current,
+              "#event-" + activeIndex + " .title",
               { y: 0, x: 0, ease: "elastic.out(1,0.5)", duration: 2 },
               "<",
             )
             .to(
-              titleRef.current,
-              { y: 0, x: 0, ease: "elastic.out(1,0.5)", duration: 2 },
-              "<",
-            )
-            .to(
-              textContainerRef.current,
+              "#event-" + activeIndex + " .textImage",
               { y: 0, x: 0, ease: "elastic.out(1,0.5)", duration: 2 },
               "<",
             );
         } else {
           gsap
             .timeline()
-            .to(textContainerRef.current, {
+            .to("#event-" + activeIndex + " .textImage", {
               y: 100,
               x: 100,
               ease: "elastic.out(1,0.5) ",
               duration: 2,
             })
-            .to(overlayRef.current, { autoAlpha: 1 }, "<")
+            .to("#eventOverlay-" + activeIndex, { autoAlpha: 1 }, "<")
             .to(
-              imageRef.current,
+              "#event-" + activeIndex + " .title",
               { y: 100, x: 100, ease: "elastic.out(1,0.5)", duration: 2 },
               "<",
             )
             .to(
-              titleRef.current,
-              { y: 100, x: 100, ease: "elastic.out(1,0.5)", duration: 2 },
-              "<",
-            )
-            .to(
-              overlayContentRef.current,
+              "#eventOverlay-" + activeIndex + " .overlayContent",
               { y: 0, x: 0, ease: "elastic.out(1,0.5) ", duration: 2 },
               "<",
             );
@@ -113,171 +287,19 @@ const Event = ({ eventContent }: { eventContent: EventContentProps }) => {
   }, [overlayIsHidden]);
 
   return (
-    <div ref={eventRef} className="shrink-0">
-      {title && (
-        <div className="pl-[28.7rem] 2xl:pl-[38rem]">
-          <h3
-            ref={titleRef}
-            className="relative z-10 pb-[3.7rem] after:absolute after:inset-x-0 after:bottom-0 after:border-l after:border-dashed after:pb-[3.7rem] after:content-['']"
-          >
-            {title}
-          </h3>
-        </div>
-      )}
-      <div className="border-t border-dashed pl-[28.7rem] 2xl:pl-[38rem]">
-        <div
-          ref={textContainerRef}
-          className="relative z-10 flex h-[50vh] gap-20 pt-[3.7rem] after:absolute after:inset-x-0 after:top-0 after:border-l after:border-dashed after:pb-[3.7rem] after:content-['']"
-        >
-          {description && (
-            <div ref={textRef} className="max-w-[30.6rem] text-white">
-              <CustomPortableText value={description as PortableTextBlock[]} />
-            </div>
-          )}
-          {coverImage && coverImage.assetPath && (
-            <Image
-              ref={imageRef}
-              src={coverImage.assetPath}
-              alt={coverImage.caption || "missing alt"}
-              width={800}
-              height={1000}
-              className="z-10 w-[40rem] cursor-pointer object-contain max-lg:translate-y-[-7.4rem] lg:pb-32"
-              onClick={handleClick}
-            />
-          )}
-        </div>
-      </div>
-      <div
-        ref={overlayRef}
-        className="invisible fixed left-0 top-0 z-20 h-screen w-screen overflow-auto bg-brown text-white md:overflow-hidden"
-      >
-        <div className="md:hidden">
-          <div className="relative">
-            {coverImage && coverImage.assetPath && (
-              <Image
-                src={coverImage.assetPath}
-                alt={coverImage.caption || "missing alt"}
-                width={1600}
-                height={2000}
-                className="w-full"
-              />
-            )}
-            {title && (
-              <div className="absolute left-[1.6rem] top-1/2 -translate-y-1/2">
-                <h1>{title}</h1>
-              </div>
-            )}
-            <span
-              className="absolute right-[1.6rem] top-1/2 -translate-y-1/2 cursor-pointer"
-              onClick={handleClick}
-            >
-              Back
-            </span>
-          </div>
-          <div className="flex -translate-y-24 flex-col gap-16 px-[4.4rem]">
-            {description && (
-              <CustomPortableText value={description as PortableTextBlock[]} />
-            )}
-            {secondaryImage && secondaryImage.assetPath && (
-              <div className="flex justify-end gap-[1.6rem]">
-                <p className="caption h-fit max-w-44 self-end italic before:content-['('] after:content-[')']">
-                  {secondaryImage.caption}
-                </p>
-                <Image
-                  src={secondaryImage.assetPath}
-                  alt={secondaryImage.caption || "missing alt"}
-                  width={240}
-                  height={320}
-                  className="w-48 object-contain"
-                />
-              </div>
-            )}
-            {content && (
-              <CustomPortableText value={content as PortableTextBlock[]} />
-            )}
-          </div>
-        </div>
-        <div className="max-md:hidden">
-          {coverImage && coverImage.assetPath && (
-            <Image
-              src={coverImage.assetPath}
-              alt={coverImage.caption || "missing alt"}
-              width={800}
-              height={1000}
-              className="absolute left-32 top-1/2 w-[40rem] -translate-y-1/2 object-contain"
-            />
-          )}
-          <div className="absolute left-[19rem] top-1/2 flex gap-16 xl:left-[38rem]">
-            <div
-              ref={overlayContentRef}
-              className="overlayContent flex translate-x-[-100px] translate-y-[-100px] gap-16 pr-[1.6rem]"
-            >
-              {description && (
-                <CustomPortableText
-                  value={description as PortableTextBlock[]}
-                />
-              )}
-              {content && (
-                <CustomPortableText value={content as PortableTextBlock[]} />
-              )}
-            </div>
-          </div>
-
-          {title && (
-            <div className="absolute left-[13.6rem] flex h-[50vh] items-end pb-[12.5rem] lg:left-[27.3rem] xl:left-[54.6rem]">
-              <h1>{title}</h1>
-            </div>
-          )}
-          {secondaryImage && secondaryImage.assetPath && (
-            <div className="absolute right-[22rem] top-[16.5rem] flex items-end gap-[1.6rem]">
-              <p className="caption max-w-44 italic before:content-['('] after:content-[')']">
-                {secondaryImage.caption}
-              </p>
-              <Image
-                src={secondaryImage.assetPath}
-                alt={secondaryImage.caption || "missing alt"}
-                width={240}
-                height={320}
-                className="w-48 object-contain"
-              />
-            </div>
-          )}
-          <span
-            className="absolute right-32 top-[12.5rem] cursor-pointer"
-            onClick={handleClick}
-          >
-            Back
-          </span>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default function Timeline({ content }: { content: TimelineProps }) {
-  const { header, events, link } = content;
-  const timelineRef = useRef<HTMLDivElement>(null);
-  const buttonRef = useRef<HTMLDivElement>(null);
-  const logoRef = useRef<HTMLDivElement>(null);
-
-  gsap.registerPlugin(ScrollTrigger);
-
-  useGSAP(() => {
-    gsap.to(timelineRef.current, {
-      xPercent: -100,
-      x: () => window.innerWidth,
-      ease: "none",
-      scrollTrigger: {
-        trigger: timelineRef.current,
-        pin: true,
-        end: () => `+=${timelineRef.current?.offsetWidth} bottom`,
-        scrub: 1,
-      },
-    });
-  });
-
-  return (
     <div className="no-scrollbar overflow-x-scroll bg-brown text-white">
+      {events &&
+        events.map((event, i) => {
+          return (
+            <EventOverlay
+              eventContent={event}
+              key={i}
+              index={i}
+              overlayIsHidden={overlayIsHidden}
+              setOverlayIsHidden={setOverlayIsHidden}
+            />
+          );
+        })}
       <div ref={timelineRef} className="relative flex h-screen w-max items-end">
         <div
           ref={logoRef}
@@ -293,7 +315,16 @@ export default function Timeline({ content }: { content: TimelineProps }) {
         <div className="flex items-end after:h-[calc(50vh+1px)] after:border-t after:border-dashed after:pr-[67.6rem] after:content-['']">
           {events &&
             events.map((event, i) => {
-              return <Event eventContent={event} key={i} />;
+              return (
+                <Event
+                  eventContent={event}
+                  key={i}
+                  index={i}
+                  overlayIsHidden={overlayIsHidden}
+                  setOverlayIsHidden={setOverlayIsHidden}
+                  setActiveIndex={setActiveIndex}
+                />
+              );
             })}
         </div>
         <div className="absolute bottom-0 right-0 mb-[16.7rem] mr-[28.7rem] h-[50vh] w-[35.4rem] 2xl:mr-[40.4rem]">
